2b167ef4158014b3630de5803abb6fbb7400f057
congzhou09 <yuancongzhou@yeah.net>
2020-02-17 11:30:25 +0800

    Site updated: 2020-02-17 11:30:23

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var VERSION = '2020-2-17 11:23:49';
+var VERSION = '2020-2-17 11:29:14';
 var CACHE_NAME_PREFIX = "congzhou-blog-cache-",
   CACHE_NAME = CACHE_NAME_PREFIX + VERSION;
 var urlsToCache = [
